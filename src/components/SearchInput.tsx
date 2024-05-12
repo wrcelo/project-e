388b062d4cf3d0db632ef8fb346317017e5b894b
wrcelo <marcelomramalho@gmail.com>
2024-05-12 16:13:56 -0300

    removing separator on mobile

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -5,7 +5,7 @@ import { SearchIcon } from "lucide-react";
 
 const SearchInput = () => {
 	return (
-		<div className=" w-full hidden md:flex">
+		<div className="w-full hidden md:flex">
 			<Input
 				placeholder="Buscar"
 				className="rounded-r-none focus-visible:ring-0 focus-visible:ring-offset-0 border border-r-0"
