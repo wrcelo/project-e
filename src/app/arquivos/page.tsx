db2865819e95e9f9e1b0087943593f7c7f4ee122
wrcelo <marcelomramalho@gmail.com>
2024-05-13 17:30:44 -0300

    muted-foreground

--- src/app/arquivos/page.tsx
+++ src/app/arquivos/page.tsx
@@ -5,7 +5,7 @@ const Arquivos = () => {
 	return (
 		<div className="p-5 md:p-8">
 			<PageHeader title={"Arquivos"} />
-			<div className="flex items-center justify-center h-[85dvh] w-dvh">Em construção.</div>
+			<div className="flex items-center justify-center h-[85dvh] w-dvh text-muted-foreground">Em construção.</div>
 		</div>
 	);
 };
