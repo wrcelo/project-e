96607a2de09b73dc6645a5d9cdef91143ffefe67
wrcelo <marcelomramalho@gmail.com>
2024-07-29 19:52:26 -0300

    ajuste vercel

--- src/app/arquivos/page.tsx
+++ src/app/arquivos/page.tsx
@@ -1,10 +1,14 @@
 import PageHeader from "@/components/PageHeader";
 import PageLayout from "@/components/PageLayout";
+import { LayoutList } from "lucide-react";
 import React from "react";
 
 const Arquivos = () => {
 	return (
-		<PageLayout displayName="Arquivos">
+		<PageLayout
+			displayName="Arquivos"
+			icon={<LayoutList className="w-4 h-4" />}
+		>
 			<div></div>
 		</PageLayout>
 	);
